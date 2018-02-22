84c49cce850504047661f11cd4f5f1087e1485d5
Kris2000Ivanov <36637251+Kris2000Ivanov@users.noreply.github.com>
2018-02-22 11:22:02 +0200

    Add files via upload

---
+++ SaveInformation.java
@@ -0,0 +1,28 @@
+package LetMeGiveItAnotherTry;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+public class SaveInformation {
+public static void main(String[] args) throws IOException {
+	Scanner input=new Scanner(System.in);
+	String name=input.next();
+	File file=new File(name);
+	System.out.println("please input a number: ");
+	int number=input.nextInt();
+	FileWriter output = new FileWriter(file, true);
+	output.append("\n"+number+"\n");
+	input.close();
+	output.close();	
+	
+	FileReader r=new FileReader();
+	System.out.println("this is your information so far:");
+	r.openFile();
+	r.readfile();
+	r.closeFile();
+	
+}
+}
